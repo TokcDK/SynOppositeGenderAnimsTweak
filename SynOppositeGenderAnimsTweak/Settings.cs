0d461c64716043bf49e6767d3b531d39fc74f84e
TokcDK <TokcDK@Gmail.com>
2022-08-02 21:47:16 +0500

    added settings option to parse only unique npcs

---
+++ SynOppositeGenderAnimsTweak/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Mutagen.Bethesda.Synthesis.Settings;
+
+namespace SynOppositeGenderAnimsTweak
+{
+    public class Settings
+    {
+        [SynthesisSettingName("Only unique npc")]
+        [SynthesisTooltip("Remove opposite gender flag only from unique npcs")]
+        [SynthesisDescription("Remove opposite gender flag only from unique npcs")]
+        public bool IsOnlyUnique { get; set; } = false;
+    }
+}
