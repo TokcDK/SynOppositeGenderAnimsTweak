c1adda9125f8e97f96fe43328ee9893a535d5874
TokcDK <TokcDK@Gmail.com>
2022-08-02 22:18:36 +0500

    corrected options description

--- SynOppositeGenderAnimsTweak/Settings.cs
+++ SynOppositeGenderAnimsTweak/Settings.cs
@@ -4,13 +4,13 @@ namespace SynOppositeGenderAnimsTweak
 {
     public class Settings
     {
-        [SynthesisSettingName("Check male npcs")]
-        [SynthesisTooltip("Check male npcs for the flag. Disabled by default. Usually flag can be added for man elves")]
-        [SynthesisDescription("Check male npcs for the flag")]
+        [SynthesisSettingName("Check Male npcs")]
+        [SynthesisTooltip("Check Male npcs for the flag. Disabled by default. Usually flag can be added for man elves.")]
+        [SynthesisDescription("Check Male npcs for the flag. Disabled by default. Usually flag can be added for man elves.")]
         public bool IsCheckMales { get; set; } = false;
-        [SynthesisSettingName("Check male npcs")]
-        [SynthesisTooltip("Check male npcs for the flag. Usually flag can be added for strong womans in heavy armor")]
-        [SynthesisDescription("Check male npcs for the flag")]
+        [SynthesisSettingName("Check Female npcs")]
+        [SynthesisTooltip("Check Female npcs for the flag. Usually flag can be added for strong womans in heavy armor.")]
+        [SynthesisDescription("Check Female npcs for the flag. Usually flag can be added for strong womans in heavy armor.")]
         public bool IsCheckFemales { get; set; } = true;
 
         [SynthesisSettingName("Only unique npc")]
